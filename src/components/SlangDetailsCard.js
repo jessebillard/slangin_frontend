7550f9cb3ceef1496f17df562f05bd230d370223
jessebillard <jessebillard@mac.com>
2018-07-19 14:35:42 -0400

    style buttons on slang details card

--- src/components/SlangDetailsCard.js
+++ src/components/SlangDetailsCard.js
@@ -8,12 +8,10 @@ const SlangDetailsCard = () => {
                 <Card.Content header='Slang Title' />
                 <Card.Content description="Description of the Slang" />
                 <Card.Content extra>
-                <br />
-                Listen
-                <Button circular icon="play circle outline">
-                    
-                </Button>
-                <Button circular content="I say this too!" />
+                    <div className='ui two buttons'>
+                        <Button inverted color="blue" circular icon="play circle outline" />                        
+                        <Button inverted color="orange" circular content="I say this too!" />  
+                    </div>
                 </Card.Content>
             </Card>
         </div>
